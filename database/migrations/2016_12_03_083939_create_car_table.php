31d66d4ec0e04a87b3551a93ce7175c7c6175ad3
Saran440 <saranp502@hotmail.com>
2016-12-03 16:21:42 +0700

    add picture_datacar

---
+++ database/migrations/2016_12_03_083939_create_car_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('car_name');
+            $table->string('brand');
+            $table->string('type');
+            $table->integer('price');
+            $table->integer('status');
+            $table->date('date_of_start');
+            $table->date('date_of_end');
+            $table->integer('idcard');
+            $table->string('name');
+            $table->integer('tel');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car');
+    }
+}
